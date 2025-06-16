a57498bedb1fbd4b14eb6bdb247f817368034b60
Beatriz Salvador Parolin <Beatriz.Parolin@ibm.com>
2025-06-16 16:32:09 -0300

    feat: starting project

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+const mockDatabase = [
+  {
+    id: 1,
+    name: "test",
+  },
+];
+
+const port = 3000;
+
+app.listen(port, () =>
+  console.log(`Server successfully running in port ${port}.`)
+);
+
+app.get("/products", (req, res) => {
+  console.log("get route!");
+});
+
+app.post("/products", (req, res) => {
+  console.log("post route!");
+});
+
+app.put("/products/query", (req, res) => {
+  console.log("put route!");
+});
+
+app.put("/products/:id", (req, res) => {
+  console.log("put route (again)!");
+});
+
+app.delete("/products/:id", (req, res) => {
+  console.log("delete route!");
+});
