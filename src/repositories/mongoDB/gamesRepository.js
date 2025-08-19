eb5f0bded2bed1b3e2bb4fd62e559a8f3501bd75
Beatriz Salvador Parolin <Beatriz.Parolin@ibm.com>
2025-08-19 18:01:08 -0300

    feat: connect to DB

--- src/repositories/mongoDB/gamesRepository.js
+++ src/repositories/mongoDB/gamesRepository.js
@@ -1,3 +1,14 @@
 const { MongoClient } = require("mongodb");
 
-const client = new MongoClient("url"); // connect to my DB
+async function main() {
+  const uri =
+    "mongodb+srv://backend-learning:nNucycs12ZtT8pSd@backend-learning.vbv3htr.mongodb.net/?retryWrites=true&w=majority&appName=backend-learning";
+
+  const client = new MongoClient(uri);
+
+  await client.connect();
+
+  await client.close();
+}
+
+main().catch(console.error);
